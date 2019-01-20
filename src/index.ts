e3c4ff2fd742d0ddb4a9547a918a659935ea54ed
Eric Nilsson <eric.nilsson@hotmail.com>
2019-01-20 22:39:40 +0100

    Add hack to apollo server to access request.body.variables in the validator

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import * as jwt from 'express-jwt';
 import queryComplexity from 'graphql-query-complexity';
 import { fieldConfigEstimator, simpleEstimator } from 'graphql-query-complexity';
 
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, GraphQLOptions } from 'apollo-server-express';
 import { formatError } from 'apollo-errors';
 import * as Express from 'express';
 import { Container } from 'typedi';
@@ -60,39 +60,15 @@ async function bootstrap() {
         });
     } catch (error) { console.log('error', error) }
 
-    const apolloServer = new ApolloServer({
+    // const apolloServer = new ApolloServer({
+    const apolloServer = new ComplexServer({ // Hackily hack-hack
         schema,
         formatError,
-        // formatError: formatArgumentValidationError,
         context: ({ req, res }: any): Context => {
             return {
                 tokenData: req.tokenData
             }
-        },
-        validationRules: [
-          queryComplexity({
-            // The maximum allowed query complexity, queries above this threshold will be rejected
-            maximumComplexity: 30,
-            // The query variables. This is needed because the variables are not available
-            // in the visitor of the graphql-js library
-            variables: {},
-            // Optional callback function to retrieve the determined query complexity
-            // Will be invoked weather the query is rejected or not
-            // This can be used for logging or to implement rate limiting
-            onComplete: (complexity: number) => {
-              console.log("Query Complexity:", complexity);
-            },
-            estimators: [
-              // Using fieldConfigEstimator is mandatory to make it work with type-graphql
-              fieldConfigEstimator(),
-              // This will assign each field a complexity of 1 if no other estimator
-              // returned a value. We can define the default value for field not explicitly annotated
-              simpleEstimator({
-                defaultComplexity: 1
-              })
-            ]
-          }) as any
-        ]
+        }
     });
 
     const app = Express();
@@ -115,3 +91,43 @@ async function bootstrap() {
 try {
     bootstrap();
 } catch (error) { console.log('error', error) }
+
+
+// XXX: Evil hack to use graphql-cost-analysis
+class ComplexServer extends ApolloServer {
+    async createGraphQLServerOptions(
+        req: Express.Request,
+        res: Express.Response
+    ): Promise<GraphQLOptions> {
+        const options = await super.createGraphQLServerOptions(req, res);
+
+        return {
+            ...options,
+            validationRules: [
+                queryComplexity(
+                    {
+                        // The maximum allowed query complexity, queries above this threshold will be rejected
+                        maximumComplexity: 30,
+                        // The query variables. This is needed because the variables are not available
+                        // in the visitor of the graphql-js library
+                        variables: req.body.variables,
+                        // Optional callback function to retrieve the determined query complexity
+                        // Will be invoked weather the query is rejected or not
+                        // This can be used for logging or to implement rate limiting
+                        onComplete: (complexity: number) => {
+                        console.log("Query Complexity:", complexity);
+                    },
+                    estimators: [
+                        // Using fieldConfigEstimator is mandatory to make it work with type-graphql
+                        fieldConfigEstimator(),
+                        // This will assign each field a complexity of 1 if no other estimator
+                        // returned a value. We can define the default value for field not explicitly annotated
+                        simpleEstimator({
+                            defaultComplexity: 1
+                        })
+                    ]
+                }) as any
+            ]
+        };
+    }
+}
