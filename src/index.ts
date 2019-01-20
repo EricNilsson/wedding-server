9d52bcf88b6a00e816365ef458d83e3722497a87
Eric Nilsson <eric.nilsson@hotmail.com>
2019-01-20 18:14:00 +0100

    Add graphql-complexity

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import * as TypeORM from 'typeorm';
 import * as TypeGraphQL from 'type-graphql';
 import * as jwt from 'express-jwt';
 
+import queryComplexity from 'graphql-query-complexity';
+import { fieldConfigEstimator, simpleEstimator } from 'graphql-query-complexity';
+
 import { ApolloServer } from 'apollo-server-express';
 import { formatError } from 'apollo-errors';
 import * as Express from 'express';
@@ -66,6 +69,30 @@ async function bootstrap() {
                 tokenData: req.tokenData
             }
         },
+        validationRules: [
+          queryComplexity({
+            // The maximum allowed query complexity, queries above this threshold will be rejected
+            maximumComplexity: 30,
+            // The query variables. This is needed because the variables are not available
+            // in the visitor of the graphql-js library
+            variables: {},
+            // Optional callback function to retrieve the determined query complexity
+            // Will be invoked weather the query is rejected or not
+            // This can be used for logging or to implement rate limiting
+            onComplete: (complexity: number) => {
+              console.log("Query Complexity:", complexity);
+            },
+            estimators: [
+              // Using fieldConfigEstimator is mandatory to make it work with type-graphql
+              fieldConfigEstimator(),
+              // This will assign each field a complexity of 1 if no other estimator
+              // returned a value. We can define the default value for field not explicitly annotated
+              simpleEstimator({
+                defaultComplexity: 1
+              })
+            ]
+          }) as any
+        ]
     });
 
     const app = Express();
