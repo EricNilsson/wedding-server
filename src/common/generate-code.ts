d030a6ccccd3a1c77259114615e01cea89fe080e
Eric Nilsson <eric.nilsson@hotmail.com>
2019-02-03 16:55:03 +0100

    Make new codes numbers instead of text

--- src/common/generate-code.ts
+++ src/common/generate-code.ts
@@ -1,3 +1,5 @@
-export default function generate(length: number = 4) {
-    return Math.random().toString(36).substring(2, length + 2).toUpperCase()
+export default function generate(length: number = 4): string {
+    const code = Math.floor(1000 + Math.random() * 9000).toString();
+    return code.replace('0', Math.floor(1 + Math.random() * 9).toString());
+    // return Math.random().toString(36).substring(2, length + 2).toUpperCase()
 }
