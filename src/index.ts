dcc62740f7f7c78180129ec8ef969ebc0bb464fe
Eric Nilsson <eric.nilsson@hotmail.com>
2019-07-16 22:28:30 +0200

    fix(jwt): listen for jwt query instead of token query

--- src/index.ts
+++ src/index.ts
@@ -87,8 +87,8 @@ async function bootstrap() {
                 getToken: (req) => {
                     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
                         return req.headers.authorization.split(' ')[1];
-                    } else if (req.query && req.query.token) {
-                        return req.query.token;
+                    } else if (req.query && req.query.jwt) {
+                        return req.query.jwt;
                     }
                     return null;
                 }
